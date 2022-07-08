ed4ddf114bb7edd3dd21c3bc2d7d514e8fef0f2a
phuongnambro <lephnam159@gmail.com>
2022-07-08 21:08:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="container">
+          <div className="header__logo">
+            <a href="/">Serendale</a>
+          </div>
+          <div className="header__navigation">
+            <ul>
+              <li>Smart Contracts</li>
+              <li>Services</li>
+              <li>Solutions</li>
+              <li>Roadmap</li>
+              <li>Whitepaper</li>
+            </ul>
+          </div>
+          <div className="header__social-link">
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-github"></i>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="container">
+          <div className="main__content">
+            <h1>A Fast Blockchain.</h1>
+            <h2>Scalable AI.</h2>
+            <p>Our technology performing fast blockchain (120K TPS) and it has guaranteed AI-based data security. Proof of Stake, its consensus algorithm enables unlimited speeds.</p>
+          </div>
+          <div className="main__button">
+            <button>Get Started</button>
+            <button>Ecosystems</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
